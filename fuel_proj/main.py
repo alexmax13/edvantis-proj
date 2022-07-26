d61865c9888262e137545e9b7a7bedd0cacafd4d
Oleksandr <oleksandrmax13@gmail.com>
2022-07-26 20:07:30 +0300

    add dict params to api function

--- fuel_proj/main.py
+++ fuel_proj/main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 from sql_app.database import DatabaseAccess
 
 
-
 app = FastAPI()
 
 @app.get("/")
-def home():
+def home(location={"latitude": 50.624613, "longitude":26.257870
+}): # validation
     db = DatabaseAccess()
-    stations = db.get_stations()
+    stations = db.get_stations(location)
     return stations
+
