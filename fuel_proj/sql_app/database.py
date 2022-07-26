65e06787268aa47a7b240c076ce19eca1011b138
Oleksandr <oleksandrmax13@gmail.com>
2022-07-26 20:41:06 +0300

    add method push_data to database class, edit method get_stations: now it work with coordinates and searches by locations

--- fuel_proj/sql_app/database.py
+++ fuel_proj/sql_app/database.py
@@ -1,3 +1,4 @@
+from operator import and_
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from . import models
@@ -15,15 +16,30 @@ DATABASE_URI = f'postgresql://{USER}:{PASSWORD}@{HOST}:5432/{NAME}'
 engine = create_engine(DATABASE_URI)
 Sesssion = sessionmaker(bind=engine)
 
-# models.Base.metadata.create_all(bind=engine)   -- create base if not exist
+models.Base.metadata.create_all(bind=engine)   # -- create base if not exist
 
 class DatabaseAccess:
-        # schema_model = schemas.Record()
 
+    def get_stations(self, locations): # add location 
+        latitude_min = locations['latitude'] - 0.5
+        latitude_max = locations['latitude'] + 0.5
 
-    def get_stations(self): # add location 
-         with Sesssion() as sess:
+        longitude_min = locations['longitude'] - 0.5
+        longitude_max = locations['longitude'] + 0.5
+
+        with Sesssion() as sess:
             result = []
-            for i in sess.query(models.Stations):
+            for i in sess.query(
+                models.Stations).filter(
+                    latitude_min <= models.Stations.latitude, 
+                    models.Stations.latitude <= latitude_max, 
+                    longitude_min <= models.Stations.longitude, 
+                    models.Stations.longitude <= longitude_max
+                    ).group_by(models.Stations.station_id):
                 result.append(i)
             return result
+
+    def push_data(self, data):
+        with Sesssion() as sess:
+            sess.add(data)
+            sess.commit()
