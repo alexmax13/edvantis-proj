b061d7fc16c81dfc78539ee4b63f917d9fb62db5
Oleksandr <oleksandrmax13@gmail.com>
2022-08-06 15:40:50 +0300

    refactor method to get json data from wog api, add feature to get information with fuel avaiability

--- fuel_proj/adapter_wog.py
+++ fuel_proj/adapter_wog.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import uuid
 from sql_app import database, models
 
 URL = "https://api.wog.ua/fuel_stations"
@@ -10,39 +9,61 @@ def adapter(url):
     data = json.loads(api_response.text)['data']
     db = database.DatabaseAccess()
     
-    fuel_info_dict = data['fuel_filters']
-    fuel_prices = {}
-    
-    for fuel_info in fuel_info_dict:
-        if fuel_info['id'] == 3:
-            fuel_prices['Gas'] = fuel_info['price']
+    with database.Sesssion() as sess:        
+        for station_data in data['stations']:
 
-        if fuel_info['id'] == 9:
-            fuel_prices['Disel'] = fuel_info['price']
+            stations = models.Stations(
+                station_name = "WOG",
+                api_id = station_data['id'],
+                address = station_data['name'],
+                longitude = station_data['coordinates']['longitude'],
+                latitude = station_data['coordinates']['latitude'])
 
-        if fuel_info['id'] == 8:
-            fuel_prices['A_92'] = fuel_info['price']
+            db.push_data(stations)
 
-        if fuel_info['id'] == 2:
-            fuel_prices['A_95'] = fuel_info['price']
-
+        for fuel_data in data['fuel_filters']:
+            if fuel_data['id'] == 3:
+                fuel_type = models.FuelType(
+                    api_id = fuel_data['id'],
+                    fuel_name = fuel_data['name'])
+                    
+            else:
+                fuel_type = models.FuelType(
+                    api_id = fuel_data['id'],
+                    fuel_name = fuel_data['name'] + " " + fuel_data['brand'])
             
-    for station_data in data['stations']:
+            db.push_data(fuel_type)
 
+        for fuel_data in data['fuel_filters']:
+                for id in sess.query(models.FuelType):
+                    if fuel_data['id'] == id.api_id:
+                        fuel_price = models.FuelPrice(
+                            fuel_id = id.id,
+                            price = round(float(fuel_data['price'] / 100), 2))
+                        db.push_data(fuel_price)
+                        break
 
-        record = models.Stations(
-
-            station_name = "WOG",
-            address = station_data['name'],
-            A_95 = fuel_prices['A_95'],
-            A_92 = fuel_prices['A_92'],
-            Disel = fuel_prices['Disel'],
-            Gas = fuel_prices['Gas'],
-            longitude = station_data['coordinates']['longitude'],
-            latitude = station_data['coordinates']['latitude']
-            )
-
-        db.push_data(record)
 
         
+        for station_data in data['stations']:
+
+            station_link = station_data['link']
+            response_station_id = requests.get(station_link)
+            current_station_data = json.loads(response_station_id.text)['data']
+
+            for statation in sess.query(models.Stations):
+                if current_station_data['id'] == statation.api_id:
+                    for type in sess.query(models.FuelType):
+                        for fuel in current_station_data['fuels']:
+                            if type.api_id == fuel['id']:
+
+                                fuel_availability = models.FuelAvailability(
+                                    station_id = statation.id,
+                                    fuel_id = type.id)
+                                    
+                                db.push_data(fuel_availability)
+                                break
+                    break
+                    
+                            
 adapter(URL)
