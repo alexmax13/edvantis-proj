e28eab341e19ccfa01a745fc231e1ca96999fb40
Oleksandr <oleksandrmax13@gmail.com>
2022-08-06 15:34:46 +0300

    add endpoints: home(with instruction), staions(get stations in range), price(get all prices)

--- fuel_proj/main.py
+++ fuel_proj/main.py
@@ -4,10 +4,30 @@ from sql_app.database import DatabaseAccess
 
 app = FastAPI()
 
+
+from pydantic import BaseModel
+
+class Location(BaseModel):
+    longitude: float 
+    latitude: float
+
+
+class Range(BaseModel):
+    range: float
+
+
 @app.get("/")
-def home(location={"latitude": 50.624613, "longitude":26.257870
-}): # validation
+def home():
+    return "go to /stations to see stations, go to /price to see prices"
+
+@app.get("/stations")  # add parametrs
+def stations(location: Location={"longitude":26.257870, "latitude": 50.624613}, range: Range=50.0): # validation
     db = DatabaseAccess()
-    stations = db.get_stations(location)
+    stations = db.get_stations(location, range)
     return stations
 
+@app.get("/price")
+def price():
+    db = DatabaseAccess()
+    price = db.get_price()
+    return price
