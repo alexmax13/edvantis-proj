1ce41397a0c147d8df8ed940f1406a4cc5d08360
Oleksandr <oleksandrmax13@gmail.com>
2022-08-13 18:57:30 +0300

    refactoring code

--- fuel_proj/adapter_wog.py
+++ fuel_proj/adapter_wog.py
@@ -6,31 +6,30 @@ from sql_app import database, models
 URL = "https://api.wog.ua/fuel_stations"
 
 class WogScrapper:
-    """WogScrapper class use to scrape data from WOG api and send to database"""
-    def __init__(self, url) -> None:
+    """WOG api scrapper that stores data in database"""
+    def __init__(self, url: str=URL) -> None:
         self.url = url
         api_response = requests.get(self.url)
         if api_response.status_code == 200:
-            self.data = json.loads(api_response.text)['data']
-            self.data_base = database.DatabaseAccess()
-
+            self.api_raw_json = json.loads(api_response.text)['data']
+            self.db_accessor = database.DatabaseAccess()
         else:
-            print("Data is empty")
+            print("API request failed")
 
     def save_stations_data(self):
-        """method to get stations data from json and save it to orm model"""
-        for station_data in self.data['stations']:
-            stations = models.Stations(
-                station_name = "WOG",
+        """parses stations data and stores to database"""
+        for station_data in self.api_raw_json['stations']:
+            station = models.Stations(
+                station_name = "WO",
                 api_id = station_data['id'],
                 address = station_data['name'],
                 longitude = station_data['coordinates']['longitude'],
                 latitude = station_data['coordinates']['latitude'])
-            self.data_base.add_station(stations)
+            self.db_accessor.add_station(station)
 
     def save_fuel_type_data(self):
         """method to get types fuel from json and save it to orm model"""
-        for fuel_data in self.data['fuel_filters']:
+        for fuel_data in self.api_raw_json['fuel_filters']:
             if fuel_data['id'] == 3:
                 fuel_type = models.FuelType(
                     api_id = fuel_data['id'],
@@ -39,21 +38,21 @@ class WogScrapper:
                 fuel_type = models.FuelType(
                     api_id = fuel_data['id'],
                     fuel_name = fuel_data['name'] + " " + fuel_data['brand'])
-            self.data_base.add_fuel_type(fuel_type)
+            self.db_accessor.add_fuel_type(fuel_type)
 
-    def save_price_data(self):
+    def save_fuel_price_data(self):
         """method to get wog prices from json and save it to orm model"""
         with database.Sesssion() as sess:
-            for fuel_data in self.data['fuel_filters']:
+            for fuel_data in self.api_raw_json['fuel_filters']:
                 for type_id in sess.query(models.FuelType):
                     if fuel_data['id'] == type_id.api_id:
                         fuel_price = models.FuelPrice(
                             fuel_id = type_id.id,
                             price = round(float(fuel_data['price'] / 100), 2))
-                        self.data_base.add_fuel_price(fuel_price)
+                        self.db_accessor.add_fuel_price(fuel_price)
                         break
 
-    def save_avaiability_data(self):
+    def save_fuel_avaiability_data(self):
         """method to get avaiable types and prices from api and save it to orm model"""
         list_of_current_stations = self.get_current_station_data()
         with database.Sesssion() as sess:
@@ -66,23 +65,16 @@ class WogScrapper:
                                     fuel_availability = models.FuelAvailability(
                                         station_id = station.id,
                                         fuel_id = fuel_type.id)
-                                    self.data_base.add_fuel_avaiability(fuel_availability)
+                                    self.db_accessor.add_fuel_avaiability(fuel_availability)
                                     break
                             break
 
     def get_current_station_data(self):
         """method make request to wog api to get info about fuel and prices at current stations"""
         list_of_station_data = []
-        for station_data in self.data['stations']:
+        for station_data in self.api_raw_json['stations']:
             station_link = station_data['link']
             response_station_id = requests.get(station_link)
             current_station_data = json.loads(response_station_id.text)['data']
             list_of_station_data.append(current_station_data)
         return list_of_station_data
-
-if __name__ == "__main__":
-    scrapper_wog = WogScrapper(URL)
-    scrapper_wog.save_stations_data()
-    scrapper_wog.save_fuel_type_data()
-    scrapper_wog.save_price_data()
-    scrapper_wog.save_avaiability_data()
