fd037dd9b2ae17f9a306a56864ff7df120f24b97
Oleksandr <oleksandrmax13@gmail.com>
2022-08-14 14:43:18 +0300

    add logging

--- fuel_proj/cron_task.py
+++ fuel_proj/cron_task.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from adapter_wog import WogScrapper
 def cron_task():
     scrapper_wog = WogScrapper()
@@ -5,5 +6,7 @@ def cron_task():
     scrapper_wog.save_fuel_type_data()
     scrapper_wog.save_fuel_price_data()
     scrapper_wog.save_fuel_avaiability_data()
+
+    print(f"Last database update - {datetime.now()}")
 
 cron_task()
